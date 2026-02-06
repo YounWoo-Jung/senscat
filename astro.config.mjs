764a19da97b2112b184d05dabdedcc1b053110c5
정연우 <ywjung@nkia.co.kr>
2026-02-06 20:01:04 +0900

    add dist file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,18 @@ export default defineConfig({
   site: 'https://senscat.com',
   integrations: [
     tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: 'ko',
-        locales: {
-          ko: 'ko',
-          en: 'en',
-        },
-      },
-    }),
+    // sitemap({
+    //   i18n: {
+    //     defaultLocale: 'ko',
+    //     locales: {
+    //       ko: 'ko',
+    //       en: 'en',
+    //     },
+    //     routing: {
+    //       prefixDefaultLocale: false
+    //     }
+    //   },
+    // }),
   ],
   output: 'static',
 });
